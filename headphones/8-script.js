0c2ace4edf116ecd6e1e1a0e7ff22e7dc860b161
zaiim100 <74555534+zaiim100@users.noreply.github.com>
2025-10-06 20:58:53 +0100

    Add files via upload

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+});
